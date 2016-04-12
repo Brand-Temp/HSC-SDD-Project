4458c645b87973bc53f6b9f0007da2c325a67ba1
Brandon Temple <Brandon Temple>
2016-04-12 20:27:03 +1000

    SDD Senior Project - Encryption and Decryption

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.encryptFromFileButton = new System.Windows.Forms.Button();
+            this.decryptFromFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(110, 14);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(279, 20);
+            this.inputBox.TabIndex = 0;
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(110, 41);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(279, 20);
+            this.keyBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(110, 67);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.outputBox.Size = new System.Drawing.Size(279, 77);
+            this.outputBox.TabIndex = 2;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(46, 150);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(125, 59);
+            this.encryptButton.TabIndex = 3;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(12, 14);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(65, 13);
+            this.inputLabel.TabIndex = 4;
+            this.inputLabel.Text = "Enter String:";
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Location = new System.Drawing.Point(12, 41);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(81, 13);
+            this.keyLabel.TabIndex = 5;
+            this.keyLabel.Text = "Encryption Key:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 67);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(88, 13);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "Encrypted String:";
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(226, 150);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(125, 59);
+            this.decryptButton.TabIndex = 7;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // encryptFromFileButton
+            // 
+            this.encryptFromFileButton.Location = new System.Drawing.Point(46, 215);
+            this.encryptFromFileButton.Name = "encryptFromFileButton";
+            this.encryptFromFileButton.Size = new System.Drawing.Size(125, 59);
+            this.encryptFromFileButton.TabIndex = 8;
+            this.encryptFromFileButton.Text = "Encrypt From File";
+            this.encryptFromFileButton.UseVisualStyleBackColor = true;
+            this.encryptFromFileButton.Click += new System.EventHandler(this.encryptFromFileButton_Click);
+            // 
+            // decryptFromFileButton
+            // 
+            this.decryptFromFileButton.Location = new System.Drawing.Point(226, 216);
+            this.decryptFromFileButton.Name = "decryptFromFileButton";
+            this.decryptFromFileButton.Size = new System.Drawing.Size(125, 58);
+            this.decryptFromFileButton.TabIndex = 9;
+            this.decryptFromFileButton.Text = "Decrypt From File";
+            this.decryptFromFileButton.UseVisualStyleBackColor = true;
+            this.decryptFromFileButton.Click += new System.EventHandler(this.decryptFromFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 314);
+            this.Controls.Add(this.decryptFromFileButton);
+            this.Controls.Add(this.encryptFromFileButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Label keyLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button encryptFromFileButton;
+        private System.Windows.Forms.Button decryptFromFileButton;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Encryption (string input)
+        {
+                        /* Get the string to encrypt: From File
+             * 
+             */
+
+            //Process input into character array
+            char[] inputArray = input.ToCharArray();
+
+            //Creat encryption key
+            var ran = new Random();
+            int[] keyArray = Enumerable.Range(0, 94).OrderBy(x => ran.Next()).ToArray();
+
+            //Convert integers in key to legal characters
+            char[] chars = new char[126];
+            for (int k = 0; k < 94; k++)
+            {
+                keyArray[k] = keyArray[k] + 33;
+                System.Console.WriteLine(k + ". " + keyArray[k]);
+                char character = (char)keyArray[k];
+                chars[k] = character;
+            }
+
+            //Encrypt string
+            for (int i = 0; i < inputArray.Length; i = i + 1)
+            {
+                int c = Convert.ToInt32(inputArray[i]);
+                if (c >= 33 && c <= 126)
+                {
+                    c = keyArray[c - 33];
+                }
+                char character = (char)c;
+                inputArray[i] = character;
+            }
+
+            //Print Output
+            string output = new string(inputArray);
+            outputBox.Text = output;
+
+            //Print Key
+            string key = new string(chars);
+            keyBox.Text = key;
+
+            
+        }
+
+        private void Decryption(string input)
+        {
+            //Process input into character array
+            char[] inputArray = input.ToCharArray();
+
+            //Read key
+            string key = keyBox.Text;
+            char[] keyArray = key.ToCharArray();
+
+            //Decrypt string
+            for (int i = 0; i < input.Length; i++)
+            {
+                int c = Convert.ToInt32(inputArray[i]);
+                for (int j = 0; j < 94; j++ )
+                    if (inputArray[i] == keyArray[j])
+                    {
+                        c = 33 + j;
+                    }
+                inputArray[i] = (char)c;
+            }
+
+            //Print Output
+            string output = new string(inputArray);
+            outputBox.Text = output;
+
+
+
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            //Get String: Manual
+            string input = inputBox.Text;
+            Encryption(input);
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            //Get String: Manual
+            string input = inputBox.Text;
+            Decryption(input);
+        }
+
+        private void encryptFromFileButton_Click(object sender, EventArgs e)
+        {
+            //Get String: From File
+            //DialogResult result = openFileDialog1.ShowDialog();
+            /*if (result == DialogResult.OK) // Test result.
+            {
+                string input = openFileDialog1.ToString;
+            }*/
+            string input = File.ReadAllText(@"C:\Users\Brandon\Documents\Visual Studio 2013\Projects\WindowsFormsApplication1\input.txt");
+            Encryption(input);
+            string output = outputBox.Text;
+            File.WriteAllText(@"C:\Users\Brandon\Documents\Visual Studio 2013\Projects\WindowsFormsApplication1\output.txt", output);
+        }
+
+        private void decryptFromFileButton_Click(object sender, EventArgs e)
+        {
+            //Get String: Manual
+            string input = File.ReadAllText(@"C:\Users\Brandon\Documents\Visual Studio 2013\Projects\WindowsFormsApplication1\input1.txt");
+            Decryption(input);
+            string output = outputBox.Text;
+            File.WriteAllText(@"C:\Users\Brandon\Documents\Visual Studio 2013\Projects\WindowsFormsApplication1\output1.txt", output);
+        }
+    }
+}
